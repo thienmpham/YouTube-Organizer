dc5fe908c7f2f5052bcb17eccddf6d0c38cdfb25
thienmpham <thienmpham@gmail.com>
2023-10-02 21:29:12 -0400

    Fixed the url
    I realized I was using quotation marks and not back tick for the template literals. Now I am trying to see if I can return the correct data in node.js and send that to client side js, if that is even possible...

--- client.js
+++ client.js
@@ -7,17 +7,23 @@ const baseUrl = 'https://youtube.googleapis.com/youtube/v3/playlists';
 const part = 'snippet';
 const channelId = 'UCt1sWh7fALveC7cI4oQ6ZMg'
 
-const url = '${baseUrl}?part=${part}&channelId=${channelId}&key=${apiKey}';
+const url = `${baseUrl}?part=${part}&channelId=${channelId}&key=${apiKey}`;
+console.log(url);
 
-// axios.get(url)
+ fetch(url)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error ('Network Response is not OK');
+        }
+
+        return response;
+    }) .then((data) =>{
+        console.log(data)
+            console.log(data.items)
+        
+    })
+
+// fetch(url)
 // .then((data) => {
-//     console.log(data);
-// })
-// .catch((error) => {
-//     console.error('Error:', error);
-// })
-
-fetch(url)
-.then((data) => {
-    console.log(data)
-});
+//     console.log(data)
+// });
