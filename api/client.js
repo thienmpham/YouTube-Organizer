086c85af4c18daf8348c90da66bf8d5dfa06b73c
thienmpham <thienmpham@gmail.com>
2023-10-12 14:12:34 -0400

    Onto the append!!!
    As of now I have successfully added my API endpoint data to my client and next is adding it to the DOM dynamically. This is according to the users' playlists :)

--- api/client.js
+++ api/client.js
@@ -1,12 +1,24 @@
+let div = document.createElement('div');
+// Select the tab div 
+const videoTab = document.querySelector('.video-tab');
+
+// Create node clone of tab div 
+let videoTabClone = videoTab.cloneNode(true);
+console.log(videoTabClone);
+
+videoTab.append(div);
+div.append(videoTabClone);
+
+
 // Fetching data from my endpoint to my client side 
 fetch('http://localhost:8000/api')
     .then(res => {
-        res.json()
+        return res.json();
     })
     .then(data => {
         console.log(data);
     }).catch (error => {
-        (console.log(error))
+        (console.log(error));
     });
     
     
