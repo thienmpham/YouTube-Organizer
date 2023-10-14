a9fb567aa511a213809272e21de3085f09d38df2
thienmpham <thienmpham@gmail.com>
2023-10-14 18:48:47 -0400

    Append success!!!
    I figured out how and why my append was not inheriting my css from the nodeClone. Now onto dynamically changing the content such as the title and playlist picture to match the users! Wish me lucky!

--- api/client.js
+++ api/client.js
@@ -1,13 +1,27 @@
 let div = document.createElement('div');
+
 // Select the tab div 
 const videoTab = document.querySelector('.video-tab');
+console.log(videoTab)
 
 // Create node clone of tab div 
 let videoTabClone = videoTab.cloneNode(true);
 console.log(videoTabClone);
 
-videoTab.append(div);
+// Select <main> 
+const main = document.querySelector('#main');
+
+
+// Assign class so I can
+// target the cloned div whenever neccessary 
+videoTabClone.id = 'videoClone';
+
+//add empty div to <main>
+main.append(div);
+// Append empty div to cloned div 
 div.append(videoTabClone);
+console.log(div)
+
 
 
 // Fetching data from my endpoint to my client side 
