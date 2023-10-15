eb8cba1da35f2f4471a0ffe379c1014ccc5c3c25
thienmpham <thienmpham@gmail.com>
2023-10-15 12:29:01 -0400

    Iterating using for loop
    I am now entering the realm of iterating through objects using a for loop. This is going to lead me into dynamically changing elements into the users own elements!!!

--- api/client.js
+++ api/client.js
@@ -7,22 +7,23 @@ console.log(videoTab)
 // Create node clone of tab div 
 let videoTabClone = videoTab.cloneNode(true);
 console.log(videoTabClone);
-
+// Assign class so I can target the cloned div whenever neccessary 
+videoTabClone.id = 'videoClone';
 // Select <main> 
 const main = document.querySelector('#main');
 
 
-// Assign class so I can
-// target the cloned div whenever neccessary 
-videoTabClone.id = 'videoClone';
-
-//add empty div to <main>
-main.append(div);
-// Append empty div to cloned div 
-div.append(videoTabClone);
-console.log(div)
 
 
+let appendVideoTab = () => {
+    //add empty div to <main>
+    main.append(div);
+    // Append empty div to cloned div 
+    div.append(videoTabClone);
+    console.log(div)
+}
+
+appendVideoTab();
 
 // Fetching data from my endpoint to my client side 
 fetch('http://localhost:8000/api')
@@ -30,9 +31,12 @@ fetch('http://localhost:8000/api')
         return res.json();
     })
     .then(data => {
+        let playlists = data.items; 
+        for (const playlist of playlists) { 
+            console.log(playlist.snippet.title)
+        }
         console.log(data);
     }).catch (error => {
         (console.log(error));
     });
-    
     
